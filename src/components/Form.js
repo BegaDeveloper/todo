eae80f558f385c016a022575b7eae047b1c41d15
BegaDeveloper <ismarbegicit@gmail.com>
2021-07-09 14:44:00 +0200

    todo

---
+++ src/components/Form.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function Form({ setInputText, todos, setTodos, inputText, setStatus }) {
+  const inputTextHandler = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+
+  //When pressed it deletes input placeholder (delete text in input)
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
+
+  //Status
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
+
+  return (
+    <form>
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i class="fas fa-plus-square"></i>
+      </button>
+      <div class="select">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
